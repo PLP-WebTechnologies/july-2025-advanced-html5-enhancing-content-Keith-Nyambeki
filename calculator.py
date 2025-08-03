1600a2434e5a03a20d1f3509d249779ad8b18064
Keith_Nyambeki <keithnyambeki03@gmail.com>
2025-08-03 16:38:51 +0300

    Add calculator with preset numbers

---
+++ calculator.py
@@ -0,0 +1,10 @@
+num1 = 2.2
+num2 = 6.2
+operation = '+'
+if operation == '+':
+    result = num1 + num2
+elif operation == '-':
+        result = num1 - num2
+else:
+    result = "Invalid operation. Please enter + or - only."
+print("Result:", result) 
